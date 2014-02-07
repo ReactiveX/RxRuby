360bac55935b44413b0063aed48e6c51b70efb7d
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-02-07 09:56:21 -0500

    Adding prelim notification testing

---
+++ test/test_notification.rb
@@ -0,0 +1,8 @@
+# Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
+
+require 'minitest/autorun'
+require 'rx'
+
+class TestNotification < MiniTest::Unit::TestCase
+
+end
