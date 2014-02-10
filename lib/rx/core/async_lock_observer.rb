00b6e6c5787e8200293d1c72e866a8378bf4e634
Matthew Podwysocki <matthew.podwysocki@gmail.com>
2014-02-10 11:50:38 -0500

    Removing unneeded monitor

--- lib/rx/core/async_lock_observer.rb
+++ lib/rx/core/async_lock_observer.rb
@@ -1,6 +1,6 @@
 # Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
-require 'monitor'
+require 'rx/concurrency/async_lock'
 require 'rx/core/observer'
 
 module RX
