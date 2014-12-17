2d83f241ec4c7c3ffabb7e579adacca195486314
Koji Arai <jca02266@gmail.com>
2014-12-17 14:43:04 +0900

    Correct to work Observable#zip
      require 'rx'
      require 'core_ext/enumerable'
    
      a = [ 4, 5, 6 ].to_observable
      b = [ 7, 8, 9 ].to_observable
    
      sub = a.zip(b).subscribe_on_next {|arr| puts arr.to_s }
      sub.unsubscribe

--- lib/rx/operators/multiple.rb
+++ lib/rx/operators/multiple.rb
@@ -670,8 +670,6 @@ module RX
       # Merges the specified observable sequences into one observable sequence by using the selector function whenever all of the observable sequences have produced an element at a corresponding index.
       def zip(*args, &result_selector)
         AnonymousObservable.new do |observer|
-          result_selector ||= lambda {|*inner_args| observer.on_next inner_args }
-
           n = args.length
 
           queues = Array.new(n) {|i| Array.new }
@@ -680,8 +678,8 @@ module RX
           next_action = lambda do |i|
             if queues.all? {|q| q.length > 0 }
               res = queues.map {|q| q.shift }
-              observer.on_next(result_selector.call *res)
-            elsif enumerable_select_with_index(queues) {|x, j| j != i } .all?
+              observer.on_next(res)
+            elsif enumerable_select_with_index(is_done) {|x, j| j != i } .all?
               observer.on_completed
             end
           end
