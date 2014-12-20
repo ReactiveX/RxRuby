c950f8fe95ccd25eab5175b55f06b48797b533c5
Koji Arai <jca02266@gmail.com>
2014-12-20 14:14:59 +0900

    Implement Observable.interval

---
+++ lib/rx/linq/observable/_observable_timer_time_span_and_period.rb
@@ -0,0 +1,20 @@
+module RX
+  class << Observable
+    private
+    def observable_timer_time_span_and_period(due_time, period, scheduler)
+      if due_time == period
+        AnonymousObservable.new do |observer|
+          scheduler.schedule_periodic_with_state(0, period,
+            lambda {|count|
+              observer.on_next(count)
+              count + 1
+            })
+        end
+      else
+        Observable.defer {
+          observable_timer_date_and_period(scheduler.now() + due_time, period, scheduler)
+        }
+      end
+    end
+  end
+end
