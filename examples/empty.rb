2adaa65594747fbc8da4ffb6556c4b7fdc37809e
Koji Arai <jca02266@gmail.com>
2014-12-25 00:04:09 +0900

    Add an example of empty

---
+++ examples/empty.rb
@@ -0,0 +1,16 @@
+require 'rx'
+
+source = RX::Observable.empty()
+
+subscription = source.subscribe(
+    lambda {|x|
+        puts 'Next: ' + x.to_s
+    },
+    lambda {|err|
+        puts 'Error: ' + err.to_s
+    },
+    lambda {
+        puts 'Completed'
+    })
+
+# => Completed
