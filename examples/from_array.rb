807aea2a0278349ca4694de3f724d26ab8d36ae3
Koji Arai <jca02266@gmail.com>
2014-12-26 23:07:44 +0900

    Add an example of from_array

---
+++ examples/from_array.rb
@@ -0,0 +1,21 @@
+require 'rx'
+
+array = [1,2,3]
+
+source = RX::Observable.from_array(array)
+
+subscription = source.subscribe(
+    lambda {|x|
+        puts 'Next: ' + x.to_s
+    },
+    lambda {|err|
+        puts 'Error: ' + err.to_s
+    },
+    lambda {
+        puts 'Completed'
+    })
+
+# => Next: 1
+# => Next: 2
+# => Next: 3
+# => Completed
