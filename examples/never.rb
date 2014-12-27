f6b43e711f809c9da1707ea7b28c14cd4d20a694
Koji Arai <jca02266@gmail.com>
2014-12-28 08:59:59 +0900

    Add an example of Observable.never

---
+++ examples/never.rb
@@ -0,0 +1,15 @@
+require 'rx'
+
+# This will never produce a value, hence never calling any of the callbacks
+source = RX::Observable.never()
+
+subscription = source.subscribe(
+    lambda {|x|
+        puts 'Next: ' + x.to_s
+    },
+    lambda {|err|
+        puts 'Error: ' + err.to_s
+    },
+    lambda {
+        puts 'Completed'
+    })
