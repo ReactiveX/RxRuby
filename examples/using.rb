8c5004e22c4ed82de9d94d7af03aedef8f479847
Koji Arai <jca02266@gmail.com>
2014-12-28 20:46:28 +0900

    Add an example of using

---
+++ examples/using.rb
@@ -0,0 +1,52 @@
+require 'rx'
+
+#  Using an AsyncSubject as a resource which supports the .dispose method
+class DisposableResource
+  def initialize(value, disposed = false)
+    @value = value
+    @disposed = disposed
+  end
+
+  def getValue
+    if @disposed
+      throw Exception.new('Object is disposed')
+    end
+    @value
+  end
+
+  def unsubscribe
+    if !@disposed
+      @disposed = true
+      @value = nil
+    end
+    puts 'Disposed'
+  end
+end
+
+source = RX::Observable.using(
+  lambda { return DisposableResource.new(42) },
+  lambda {|resource|
+    subject = RX::AsyncSubject.new
+    subject.on_next(resource.getValue)
+    subject.on_completed
+    return subject
+  }
+)
+
+subscription = source.subscribe(
+  lambda {|x|
+    puts 'Next: ' + x.to_s
+  },
+  lambda {|err|
+    puts 'Error: ' + err.to_s
+  },
+  lambda {
+    puts 'Completed'
+  })
+
+# => Next: 42
+# => Completed
+
+subscription.dispose()
+
+# => Disposed
