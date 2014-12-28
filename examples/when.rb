53d30125512d61b430f05a698f98e1b03dc71def
Koji Arai <jca02266@gmail.com>
2014-12-28 23:47:37 +0900

    Add an example of when

---
+++ examples/when.rb
@@ -0,0 +1,26 @@
+require 'rx'
+
+# Fire each plan when both are ready
+source = RX::Observable.when(
+  RX::Observable.timer(0.1).and(RX::Observable.timer(0.5)).then_do(lambda {|x, y| return 'first' }),
+  RX::Observable.timer(0.4).and(RX::Observable.timer(0.3)).then_do(lambda {|x, y| return 'second' })
+)
+
+subscription = source.subscribe(
+  lambda {|x|
+      puts 'Next: ' + x.to_s
+  },
+  lambda {|err|
+      puts 'Error: ' + err.to_s
+  },
+  lambda {
+      puts 'Completed'
+  })
+
+# => Next: second
+# => Next: first
+# => Completed
+
+while Thread.list.size > 1
+  (Thread.list - [Thread.current]).each &:join
+end
