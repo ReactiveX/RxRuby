dfd14aa92728076a04505d4880690308a660b097
Koji Arai <jca02266@gmail.com>
2014-12-28 20:08:53 +0900

    Add an example of timer

---
+++ examples/timer.rb
@@ -0,0 +1,22 @@
+require 'rx'
+
+source = RX::Observable.timer(0.2, 0.1)
+    .time_interval
+    .pluck('interval')
+    .take(3)
+
+subscription = source.subscribe(
+    lambda {|x|
+        puts 'Next: ' + x.to_s
+    },
+    lambda {|err|
+        puts 'Error: ' + err.to_s
+    },
+    lambda {
+        puts 'Completed'
+    })
+
+# => Next: 0.2
+# => Next: 0.1
+# => Next: 0.1
+# => Completed
