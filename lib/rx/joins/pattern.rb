aa30c1b4567cc2f43c25ad3bc62e596b31120709
Koji Arai <jca02266@gmail.com>
2014-12-29 08:56:53 +0900

    Implement JoinsObserver, Pattern etc

---
+++ lib/rx/joins/pattern.rb
@@ -0,0 +1,14 @@
+module RX
+  class Pattern
+    attr_reader :patterns
+    def initialize(patterns)
+      @patterns = patterns
+    end
+    def and(other)
+      Pattern.new(@patterns.concat(other))
+    end
+    def then_do(selector)
+      Plan.new(self, selector)
+    end
+  end
+end
