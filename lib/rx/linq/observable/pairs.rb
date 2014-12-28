c496ee46c63f6fcc232342392e640c68dcf1e863
Koji Arai <jca02266@gmail.com>
2014-12-28 14:56:01 +0900

    Re-implement the Observable.pairs using Observable.of_enumerable

--- lib/rx/linq/observable/pairs.rb
+++ lib/rx/linq/observable/pairs.rb
@@ -1,21 +1,7 @@
 module RX
   class <<Observable
     def pairs(obj, scheduler = CurrentThreadScheduler.instance)
-      AnonymousObservable.new {|observer|
-        idx = 0
-        keys = obj.keys
-        len = keys.length
-        scheduler.schedule_recursive lambda {|this|
-          if idx < len
-            key = keys[idx]
-            idx += 1
-            observer.on_next [key, obj[key]]
-            this.call
-          else
-            observer.on_completed
-          end
-        }
-      }
+      of_enumerable(obj, scheduler)
     end
   end
 end
