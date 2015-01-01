d47541038ade6d376ab8534de0fced4fa2f6f7ec
Koji Arai <jca02266@gmail.com>
2015-01-01 10:50:53 +0900

    Add an example of reduce

---
+++ examples/reduce.rb
@@ -0,0 +1,18 @@
+require 'rx'
+
+source = RX::Observable.range(1, 3)
+    .reduce(1) {|acc, x| acc * x }
+
+subscription = source.subscribe(
+    lambda {|x|
+        puts 'Next: ' + x.to_s
+    },
+    lambda {|err|
+        puts 'Error: ' + err.to_s
+    },
+    lambda {
+        puts 'Completed'
+    })
+
+# => Next: 6
+# => Completed
