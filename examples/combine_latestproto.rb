07b85279a363a348b514d4cf61281fe9dc793354
Koji Arai <jca02266@gmail.com>
2015-01-02 22:40:49 +0900

    Add an example of Observable#combine_latest

---
+++ examples/combine_latestproto.rb
@@ -0,0 +1,33 @@
+require 'rx'
+
+#  Have staggering intervals
+source1 = RX::Observable.interval(0.1)
+    .map {|i| 'First: ' + i.to_s }
+
+source2 = RX::Observable.interval(0.15)
+    .map {|i| 'Second: ' + i.to_s }
+
+# Combine latest of source1 and source2 whenever either gives a value
+source = source1.combine_latest(source2) {|s1, s2| s1 + ', ' + s2 }
+    .take(4)
+
+subscription = source.subscribe(
+    lambda {|x|
+        puts 'Next: ' + x.to_s
+    },
+    lambda {|err|
+        puts 'Error: ' + err.to_s
+    },
+    lambda {
+        puts 'Completed'
+    })
+
+# => Next: First: 0, Second: 0
+# => Next: First: 1, Second: 0
+# => Next: First: 1, Second: 1
+# => Next: First: 2, Second: 1
+# => Completed
+
+while Thread.list.size > 1
+  (Thread.list - [Thread.current]).each &:join
+end
