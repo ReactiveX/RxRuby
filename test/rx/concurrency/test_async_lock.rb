2f2a60cc845f9dc1f2b12b126511420461fc1991
Tina Wuest <tina@wuest.me>
2015-03-29 22:23:30 -0500

    Refactor tests for AsyncLock

--- test/rx/concurrency/test_async_lock.rb
+++ test/rx/concurrency/test_async_lock.rb
@@ -1,64 +1,56 @@
 # Copyright (c) Microsoft Open Technologies, Inc. All rights reserved. See License.txt in the project root for license information.
 
 require 'test_helper'
-require 'thread'
 
 class TestAsyncLock < Minitest::Test
+  def setup
+    @lock = RX::AsyncLock.new
+  end
 
   def test_simple_wait
-    lock = RX::AsyncLock.new
     called = false
-    lock.wait { called = true }
-    assert called
+    @lock.wait { called = true }
+    assert_equal(true, called)
   end
 
   def test_parallel_wait
-    lock = RX::AsyncLock.new
-    called1 = false
-    called2 = false
-
-    q1 = Queue.new
-    q2 = Queue.new
+    state = [false, false]
+    sync  = [Queue.new, Queue.new]
 
     thread1 = Thread.new do
-      assert q1.pop == 1
-      # 1
-      lock.wait do
-        q2.push 2
-        assert q1.pop == 4
-        # 4
-        called1 = true
-        # 5
+      sync[0].pop
+      @lock.wait do
+        sync[1].push 1
+        sync[0].pop
+        state[0] = true
       end
-      # 8
-      assert called1
-      assert called2
+      state.each { |s| assert_equal(true, s) }
     end
 
     thread2 = Thread.new do
-      assert q2.pop == 2
-      # 2
-      lock.wait do
-        # 6
-        assert Thread.current == thread1
-        called2 = true
-        # 7
+      sync[1].pop
+      @lock.wait do
+        assert_equal(thread1, Thread.current)
+        state[1] = true
       end
-      # 3
-      assert !called1
-      assert !called2
-      q1.push 4
+      state.each { |s| assert_equal(false, s) }
+      sync[0].push 1
     end
 
-    q1.push 1
+    sync[0].push 1
     [thread1, thread2].each(&:join)
   end
 
   def test_clear
-    lock = RX::AsyncLock.new
-    lock.clear
+    @lock.clear
     called = false
-    lock.wait { called = true }
-    assert !called
+    @lock.wait { called = true }
+    assert_equal(false, called)
+  end
+
+  def test_exceptions_bubble_up_and_fault
+    assert_raises(StandardError) { @lock.wait { raise StandardError } }
+    assert_equal(true, @lock.send(:instance_variable_get, :@has_faulted))
+    assert_equal([], @lock.send(:instance_variable_get, :@queue))
   end
 end
